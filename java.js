4b80478b9eec9a95761518a7591665ff0d0cf42b
angelorel <119636277+angelorel@users.noreply.github.com>
2022-12-05 20:35:30 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,115 @@
+function start (){
+
+		let disable = document.getElementById('inicio');
+		disable.style.display="none"
+		let noview = document.getElementById('detalles');
+		noview.style.display="none"
+		let angel = document.getElementById('aserca_de');
+		angel.style.display="none"
+        let local = document.getElementById('tipos');
+		local.style.display="none"
+		let dispon1 = document.getElementById('dispon');
+		dispon1.style.display="none"
+
+
+		    let lave = document.getElementById('cuadro');
+		lave.style.display="none"
+		 let lav = document.getElementById('cuad');
+		lav.style.display="none"
+
+
+	}
+	function ini(){
+let disable = document.getElementById('inicio');
+		disable.style.display=""
+		let noview = document.getElementById('detalles');
+		noview.style.display="none"
+		let angel = document.getElementById('aserca_de');
+		angel.style.display="none"
+        let local = document.getElementById('tipos');
+		local.style.display="none"
+		let dispon1 = document.getElementById('dispon');
+		dispon1.style.display="none"
+		let iniciando = document.getElementById('contenedor');
+		iniciando.style.display="none"
+		
+		  
+
+			  let cua = document.getElementById('cuadro');
+		cua.style.display=""
+		 let lav = document.getElementById('cuad');
+		lav.style.display=""
+
+
+
+
+
+
+
+	}
+	function det(){
+		let disable = document.getElementById('inicio');
+		disable.style.display="none"
+		let noview = document.getElementById('detalles');
+		noview.style.display=""
+		let angel = document.getElementById('aserca_de');
+		angel.style.display="none"
+        let local = document.getElementById('tipos');
+		local.style.display="none"
+		let dispon1 = document.getElementById('dispon');
+		dispon1.style.display="none"
+	}
+
+
+function aserca_de(){
+		let disable = document.getElementById('inicio');
+		disable.style.display="none"
+		let noview = document.getElementById('detalles');
+		noview.style.display="none"
+		let angel = document.getElementById('aserca_de');
+		angel.style.display=""
+        let local = document.getElementById('tipos');
+		local.style.display="none"
+		let dispon1 = document.getElementById('dispon');
+		dispon1.style.display="none"
+	}
+	function tipo(){
+		let disable = document.getElementById('inicio');
+		disable.style.display="none"
+		let noview = document.getElementById('detalles');
+		noview.style.display="none"
+		let angel = document.getElementById('aserca_de');
+		angel.style.display="none"
+        let tip = document.getElementById('tipos');
+        tip.style.display=""
+        
+
+		let dispon1 = document.getElementById('dispon');
+		dispon1.style.display="none"
+
+	}
+	
+
+
+
+
+
+
+	
+	
+
+
+	
+	
+	function dispo(){
+		let disable = document.getElementById('inicio');
+		disable.style.display="none"
+		let noview = document.getElementById('detalles');
+		noview.style.display="none"
+		let angel = document.getElementById('aserca_de');
+		angel.style.display="none"
+        let local = document.getElementById('tipos');
+		local.style.display="none"
+		let dispon1 = document.getElementById('dispon');
+		dispon1.style.display=""
+	}
